5793ac9b526b2965699fb8c639da79be5e76eb38
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-02-13 15:47:58 -0800

    Added manifest and improved mobile responsiveness

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 import './style.css'
 
+const app = document.getElementById('app')
 
+// Responsive Height
+let vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
 
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+})
