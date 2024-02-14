5c04b5d45adb0ab495e3e55c33cb2e46ae828573
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-02-14 13:57:35 -0800

    Added debounce to screen height adjustment
    Will no longer make 500 calls when adjusting the height of the app.

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
 import './style.css'
 
-const app = document.getElementById('app')
-
 // Responsive Height
 let vh = window.innerHeight * 0.01
 document.documentElement.style.setProperty('--vh', `${vh}px`)
 
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01
+const debounce = (fn, delay) => {
+    let id
+    return (...args) => {
+        if (id) clearTimeout(id)
+        id = setTimeout(() => {
+            fn(...args)
+        }, delay)
+    }
+}
+
+window.addEventListener('resize', debounce(() => {
+    vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
-})
+    console.log('iv been called')
+}, 50)
+)
+
+// TEST
+// const throttle = (fn, delay) => {
+//     let lastTime = 0
+//     console.log('throttle called')
+//     return (...args) => {
+//         const now = new Date().getTime()
+//         if (now - lastTime < delay) return
+//         lastTime = now
+//         fn(...args)
+//     }
+// }
+
+// window.addEventListener('resize', throttle(()=>{
+//         vh = window.innerHeight * 0.01
+//         document.documentElement.style.setProperty('--vh', `${vh}px`)
+//         console.log('iv been called')
+//     }, 1000)
+// )
