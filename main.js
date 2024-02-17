043319663f185b0c2b99c10068d9a5195312567e
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-02-17 12:39:33 -0800

    Connected app to Color Api
    https://www.thecolorapi.com/docs

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
 import './style.css'
 
+// Set random color on page load
+document.getElementById('color-picker').value = randomHexColorCode()
+
+// Global variables
+const colorForm = document.getElementById('color-form')
+let formColor = document.getElementById('color-picker').value.replace('#', '')
+let formScheme = document.getElementById('color-schemes').value
+
+// Update color after form submit
+colorForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    formColor = document.getElementById('color-picker').value.replace('#', '')
+    formScheme = document.getElementById('color-schemes').value
+    const formData = {
+        color: formColor,
+        scheme: formScheme
+    }
+    displayColors(formData.color, formData.scheme)
+    
+})
+
+// Fetch colors from API
+function displayColors(color, scheme = 'monochrome') {
+    fetch(`https://www.thecolorapi.com/scheme?mode=${scheme}&hex=${color}`, { method: "GET" })
+    .then(res => res.json())
+    .then(data => {
+        const dataColors = data.colors.map(color => color.hex.value)
+        const displayColors = document.querySelectorAll("[data-display-colors]")
+        const displayText = document.querySelectorAll("[data-display-text]")
+        displayColors.forEach((display, index) => {
+            display.style.backgroundColor = dataColors[index]
+            displayText[index].textContent = dataColors[index]
+
+        })
+    })
+}
+
+//Generate random color
+function randomHexColorCode() {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16)
+    return '#' + n.slice(0, 6)
+}
+
 // Responsive Height
 let vh = window.innerHeight * 0.01
 document.documentElement.style.setProperty('--vh', `${vh}px`)
@@ -21,7 +64,27 @@ window.addEventListener('resize', debounce(() => {
 }, 50)
 )
 
-// TEST
+displayColors(formColor, formScheme)
+
+//DIFFERENT DEBOUNCE METHODS//////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+// TEST USING TIMEOUT ONLY ///////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+// window.addEventListener('resize', () => {
+//     let resizeTimer
+//     clearTimeout(resizeTimer)
+//     resizeTimer = setTimeout(() => {
+//         vh = window.innerHeight * 0.01
+//         document.documentElement.style.setProperty('--vh', `${vh}px`)
+//         console.log('iv been called')
+//     }, 2000)
+// })
+//////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////
+// TEST USING THROTTLE ///////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 // const throttle = (fn, delay) => {
 //     let lastTime = 0
 //     console.log('throttle called')
@@ -32,10 +95,10 @@ window.addEventListener('resize', debounce(() => {
 //         fn(...args)
 //     }
 // }
-
 // window.addEventListener('resize', throttle(()=>{
 //         vh = window.innerHeight * 0.01
 //         document.documentElement.style.setProperty('--vh', `${vh}px`)
 //         console.log('iv been called')
 //     }, 1000)
 // )
+/////////////////////////////////////////////////////////////////////////
